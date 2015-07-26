feefea88140ad9779c10d7c39f254853494bffd3
Garusis Boe <garusis@gmail.com>
2015-07-26 15:32:09 -0500

    New main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Marcos J. Alvarez on 12/07/2015.
+ * @author Marcos J. Alvarez
+ * @email garusis@gmail.com
+ * @version 0.0.3
+ */
+module.exports = function (sails) {
+    return {
+
+    };
+};
